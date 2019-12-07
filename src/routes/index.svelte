31c333cd349d5bdb0ce091cc56ee5a308eb00aeb
Wolfr <johan.ronsse@gmail.com>
2019-12-07 20:52:59 +0100

    Add more docs
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,40 @@
 <svelte:head>
-    <title>My app</title>
+    <title>Ygdir - Documentation</title>
 </svelte:head>
 
 <div class="c-container">
 
     <div class="c-content">
-        <h1>Ygdir demo</h1>
-        <p>Ygdir is a new CSS framework.</p>
+        <h1>Ygdir - Documentation</h1>
+        <p>Ygdir is a new BEM/ITCSS based framework.</p>
         <p>This page serves as a demo of Ygdir's components.</p>
     </div>
 
     <h3 class="c-h3">Buttons</h3>
+
     <p>
         <a href="/" class="c-button c-button--default">Default button</a>
         <a href="/" class="c-button c-button--primary">Primary button</a>
     </p>
 
+    <pre>
+        <code>
+            &lt;a href=&quot;/&quot; class=&quot;c-button c-button--default&quot;&gt;Default button&lt;/a&gt;
+            &lt;a href=&quot;/&quot; class=&quot;c-button c-button--primary&quot;&gt;Primary button&lt;/a&gt;
+        </code>
+    </pre>
+
+    <h3 class="c-h3">Container</h3>
+
+    <pre>
+        <code>
+            &lt;div class=&quot;c-container&quot;&gt;
+            &lt;/div&gt;
+        </code>
+    </pre>
+
     <h3 class="c-h3">Block list</h3>
+
     <p>
         ...
     </p>
