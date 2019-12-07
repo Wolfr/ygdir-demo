53c2d852f21f58e1796a7f545d9a59843ab93499
Wolfr <johan.ronsse@gmail.com>
2019-12-07 19:29:32 +0100

    Ygdir demo

---
+++ src/components/Nav.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let segment;
+</script>
+
+<nav>
+    <ul>
+        <li><a class:selected='{segment === undefined}' href='.'>home</a></li>
+    </ul>
+</nav>
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<style lang="scss" global>
+     @import "./style/app.scss";
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
